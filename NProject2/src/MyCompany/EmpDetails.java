b608b3c26f1c15617f5f74f132e209d91e38c6c1
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2023-01-31 12:05:28 +0530

    Add files via upload

---
+++ NProject2/src/MyCompany/EmpDetails.java
@@ -0,0 +1,30 @@
+package MyCompany;
+import java.sql.*;
+public class EmpDetails {
+
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		String url="jdbc:mysql://localhost:3306/employyy";
+		String uname="root";
+		String pass="manager";
+		String query="select * from employyy";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(url,uname,pass);
+		Statement st=con.createStatement();
+		ResultSet rs=st.executeQuery(query);
+			
+		rs.next();
+		String name=rs.getString("name");
+		String mobile=rs.getString("mobile");
+		String email=rs.getString("email");
+		String city=rs.getString("city");
+		System.out.println(name);
+		System.out.println(mobile);
+		System.out.println(email);
+		System.out.println(city);
+		st.close();
+		con.close();
+	}
+
+}
