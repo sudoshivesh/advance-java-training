21bf8db7b680a3192b2bd59dfc668bb74ccc804a
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2023-01-31 12:06:24 +0530

    Add files via upload

---
+++ SpringConstruction/src/com/testconnection/Test.java
@@ -0,0 +1,14 @@
+package com.testconnection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+		Student stu=(Student)context.getBean("student");
+		System.out.println(stu);
+		//((AbstractApplicationContext) context).close());
+	}
+
+}
